58b79e1f8924650fcb04914b862a98282804db15
Lukaszgor <lukasz.gg13@gmail.com>
2023-03-24 23:36:01 +0100

    supabase connection

--- irregularverbs/src/App.js
+++ irregularverbs/src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import supabase from "../src/supabaseClient"
 function App() {
+
+console.log(supabase)
+
   return (
     <div className="App">
       <header className="App-header">
