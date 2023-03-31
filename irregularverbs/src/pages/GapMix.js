21c67dab5af60502cc087287b6bf2687ce2f33dd
Lukaszgor <lukasz.gg13@gmail.com>
2023-03-31 13:43:33 +0200

    Routing

---
+++ irregularverbs/src/pages/GapMix.js
@@ -0,0 +1,14 @@
+import { useNavigate } from "react-router-dom"
+import { useState,useEffect } from 'react';
+import supabase from "../supabaseClient"
+
+const GapMix=()=>{
+
+    return(
+    <div>
+<h1>GapMix</h1>
+    </div>
+        
+    )
+}
+export default GapMix
